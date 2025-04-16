9edd93d9c7d2df80ae4bf9208ce3ede5febf34b2
Ivan Lemeshkin <106601642+LemeshkinIvan@users.noreply.github.com>
2025-04-16 22:17:36 +0300

    Delete lib/data directory

--- lib/data/repository/schedule_repository.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:tv_shedule/data/schedule.dart';
-import 'package:tv_shedule/utils/api_result.dart';
-import 'package:tv_shedule/utils/result.dart';
-
-abstract class ScheduleRepository extends ChangeNotifier{
-  Future<ApiResult<ScheduleDto>> onGetScheduleByNetwork();
-  Future<Result<ScheduleDto>> onGetScheduleFromDisk(String path);
-}
